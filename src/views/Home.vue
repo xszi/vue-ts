093e600b2cfad908687e483db7d6f73f37140835
xszi <jiuhuaw@126.com>
2021-02-23 15:41:41 +0800

    tab cop

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+      <ul>
+          <li><router-link to="/tab-view">tab组件</router-link></li>
+      </ul>
     <ChildTest />
   </div>
 </template>
 
 <script>
 // @ is an alias to /srccc
-import HelloWorld from "@/components/HelloWorld.vue";
 import ChildTest from "@/components/ChildTest.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld,
     ChildTest
   }
 };
