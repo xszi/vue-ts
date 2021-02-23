5362c467f256f89685d004787232b4cc3033b624
xszi <jiuhuaw@126.com>
2021-02-23 16:47:42 +0800

    tab cop

--- src/views/TabView.vue
+++ src/views/TabView.vue
@@ -3,6 +3,8 @@
     <h3>这是Tab组件测试页面</h3>
     <h5>tab文字后面带数字，但是bar需要停留在文字中间的下方</h5>
     <TabNav :tabs="tabs" />
+    <img src="">
+    <a href="https://github.com/xszi/vue-ts/tree/main/src/components/tabNav" target="_blank">源码地址</a>
   </div>
 </template>
 
@@ -20,19 +22,19 @@ import { Component, Vue } from "vue-property-decorator";
 })
 
 export default class TabView extends Vue {
-        tabs: object[] = [{
-            name: '环境信息',
-            num: 1,
-            tabName: 'envInfo'
-        }, {
-            name: '组件配置',
-            num: 999,
-            tabName: 'compoSet'
-        }, {
-            name: '数据库配置',
-            num: 10,
-            tabName: 'databaseSet'
-        }]
+    tabs: object[] = [{
+        name: '环境信息',
+        num: 1,
+        tabName: 'envInfo'
+    }, {
+        name: '组件配置',
+        num: 999,
+        tabName: 'compoSet'
+    }, {
+        name: '数据库配置',
+        num: 10,
+        tabName: 'databaseSet'
+    }]
 }
 </script>
 
