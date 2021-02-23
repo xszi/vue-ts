7b20f4ca0cc126308b45d8785f281b527880fdd7
xszi <jiuhuaw@126.com>
2021-02-23 17:02:03 +0800

    tab cop

--- src/views/TabView.vue
+++ src/views/TabView.vue
@@ -3,8 +3,10 @@
     <h3>这是Tab组件测试页面</h3>
     <h5>tab文字后面带数字，但是bar需要停留在文字中间的下方</h5>
     <TabNav :tabs="tabs" />
-    <img src="">
-    <a href="https://github.com/xszi/vue-ts/tree/main/src/components/tabNav" target="_blank">源码地址</a>
+    <div class="desc">
+        <img src="../assets/tabbar.png">
+        <a href="https://github.com/xszi/vue-ts/tree/main/src/components/tabNav" target="_blank">源码地址</a>
+    </div>
   </div>
 </template>
 
@@ -41,4 +43,13 @@ export default class TabView extends Vue {
 <style lang="stylus" scoped>
 h5
     color: red;
+.desc 
+    margin-top: 20px
+    display: flex;
+    flex-direction: column
+    img
+        display: inline-block
+        width: 300px
+    a
+        margin-left: 20px
 </style>
